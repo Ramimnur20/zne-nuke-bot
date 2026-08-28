02f4007f0a1c9e371384d91fc2538c2d07605f29
Mr Stonkles <voby@waifu.club>
2026-08-28 13:06:52 +0600

    remove unused imports in main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import asyncio
-import os
-
-from config import BLACKLISTED_GUILD_ID, OWNER_ID, TOKEN
+from config import TOKEN
 from core.bot import bot
-from core.operation import leave_all_servers
-from core.utils import is_premium_user
-
 
 if __name__ == "__main__":
     bot.remove_command("help")
