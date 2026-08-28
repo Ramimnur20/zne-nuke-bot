4bfaa47e467d6cbfd41087f34aa500590a62cd75
Mr Stonkles <voby@waifu.club>
2026-08-28 13:17:40 +0600

    Add: config.example.py

---
+++ config.example.py
@@ -0,0 +1,29 @@
+import discord
+
+intents = discord.Intents.all()
+
+leaderboard_message = None
+NUKE_STATS_FILE = "nuke_stats.json"
+PREMIUM_FILE = "data/premium.json"
+PREM = 0
+MOD_ROLE_ID = 0
+WHITELIST = [
+    0,
+    0,
+    0
+]
+BLACKLISTED_GUILD_ID = 0
+OWNER_ID = 0
+LEADERBOARD_CHANNEL_ID = 0
+TOKEN = 'MTQ.....'
+LOG_WEBHOOK_URL = ''
+
+BLOCKED_BOT_IDS = [
+    0
+]
+
+BLOCKED_BOT_NAMES = [
+    "Example"
+]
+
+leave_hook = ""
